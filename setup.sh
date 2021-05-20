f4fa8f4f8bc808edf7f9975007539da23bd6ab04
Fran Leston <lestonramos@gmail.com>
2021-05-20 12:27:05 +0200

    colors

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 echo "\
+[theme]
+primaryColor = '#00cb56'
+backgroundColor = 'rgb(14, 17, 23)'
+secondaryBackgroundColor = '#31333F'
+textColor= 'rgb(250, 250, 250)'
+font = 'sans serif'
 [server]\n\
 headless = true\n\
 port = $PORT\n\
